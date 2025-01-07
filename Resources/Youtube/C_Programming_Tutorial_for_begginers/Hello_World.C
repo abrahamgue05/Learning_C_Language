9c40d56ac40169b52d56d9d73cee67e8682417b9
abraham05 <abrahamgue05@gmail.com>
2025-01-06 18:07:58 -0600

    Hello World

--- Resources/Youtube/C_Programming_Tutorial_for_begginers/Hello_World.C
+++ Resources/Youtube/C_Programming_Tutorial_for_begginers/Hello_World.C
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+   printf("Hello, World!\n");
+   printf("Hello, World!\n");
    return 0;
 }
