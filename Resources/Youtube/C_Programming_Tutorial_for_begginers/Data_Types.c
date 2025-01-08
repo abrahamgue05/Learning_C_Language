8d1516a33f05ca76104c71a920786510aafbe96b
abraham05 <abrahamgue05@gmail.com>
2025-01-07 18:34:34 -0600

    Data Types

---
+++ Resources/Youtube/C_Programming_Tutorial_for_begginers/Data_Types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    int age = 40; //Integers 
+    double gpa = 40.0; //Decimal
+    char grade = 'A';//Character, store just 1 character
+    char phrase[] = "Hello world";//String of caracters
+
+    return 0;
+}
