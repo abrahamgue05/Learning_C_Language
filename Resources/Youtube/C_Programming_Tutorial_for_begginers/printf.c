6f4fb838c4088745c6e6b0406f47545a896e2872
abraham05 <abrahamgue05@gmail.com>
2025-01-07 18:49:21 -0600

    Printf

---
+++ Resources/Youtube/C_Programming_Tutorial_for_begginers/printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    double favNum = 90.1;
+    char myChar = 'i';
+
+    printf("My favorite %c is %f", myChar, favNum); //Print what is inside the ()
+    //order it will appeart on screen
+    //%d integers
+    //%s Text
+    //%f decimals/double
+    //%c characters 
+    return 0;
+}
