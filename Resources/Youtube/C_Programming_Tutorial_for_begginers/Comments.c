92eb59240708aead426be5a51da22ba5fd293e53
abraham05 <abrahamgue05@gmail.com>
2025-01-08 21:30:32 -0600

    Comments

---
+++ Resources/Youtube/C_Programming_Tutorial_for_begginers/Comments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //Coment:Block of code that gets ignored when we run a program
+    /*
+    Multiline comments, can storage more than one 
+    line of comments. Open /* Close */
+    //Single line comment, ca storage one line of comments
+    printf("Comments are fun");
+    return 0;
+}
