1c03bed970a0c5de8f768ba88fe1c52ee1562e8c
abraham05 <abrahamgue05@gmail.com>
2025-01-10 13:15:32 -0600

    If statements

---
+++ Youtube/C_Programming_Tutorial_for_begginers/If_Statements.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int max(int num1, int num2, int num3){
+    int result;
+    if(num1 >= num2 && num1 >= num3){ //if(condition),{code} //&& Logical operetor AND,
+    both conditions are TRUE
+        result = num1;
+    }else if(num2 >= num1 && num2 >= num3){ //Check another condition if the first is false
+        result = num2;
+    }else{ //execute if the condition is false
+        result = num3;
+    }
+    return result;
+}*/
+
+
+int main() {
+    // Programming structure which we can use to help the program to make decision
+    //printf("%d", max(40,10,15));
+
+    if(3 > 2 || 2 > 5){ //|| Logical operetor OR, one of the conditions has to be TRUE
+        printf("True");
+    }else{
+        printf("False");
+    }
+    return 0;
+    //== Check if conditions are equal
+    /*!= Check if conditions is not equal
+      !  Negation operator*/
+
+} 
