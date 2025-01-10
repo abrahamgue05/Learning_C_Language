60b12d6de5c0628cc551abfcbbd5bcdfca1b76ca
abraham05 <abrahamgue05@gmail.com>
2025-01-10 12:49:20 -0600

    Modified archive order

---
+++ Youtube/C_Programming_Tutorial_for_begginers/Working_With_Numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("%d \n", 5 + 5); //Int operation int = integer
+    printf("%f \n", 6.7 - 4); //Int operation double = double
+    printf("%f \n", sqrt(4));
+    printf("%f", ceil(45.455)); 
+
+    //Operations
+        //Adition +
+        //Subtraction -
+        //Division /
+        //Multiplication *
+
+    //Functions
+        //pow = Raice
+        //sqrt = Square
+        //Ceil = Round up
+        //Floor = Round down
+    return 0;
+}
