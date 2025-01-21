51635a8ddcaf63ea7550754e3ea7c3c52922b6c0
abraham05 <abrahamgue05@gmail.com>
2025-01-20 22:05:49 -0600

    Strings, saw a way to access each char address

---
+++ Youtube/CS50x_2024_Lectures/Lecture_4_Memory/Strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){\
+
+    /* & What address is the variable.
+       * Take and address and go to it
+       Pointer address of some variable that can be sabe in another variable.
+       A string is a pointrt*/
+    char *s = "HI!";//string s
+    printf("%s\n", s); //HI!
+    printf("%p\n", s);// 0x651f86241004
+    printf("%p\n", &s[0]); //0x651f86241004
+    printf("%p\n", &s[1]); //0x651f86241005
+    printf("%p\n", &s[2]); //0x651f86241006
+    printf("%p\n", &s[3]); //0x651f86241007
+}
