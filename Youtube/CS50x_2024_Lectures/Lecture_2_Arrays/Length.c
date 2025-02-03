c726e1d0e4324fb9188a04d047fd129b29c7cb97
abraham05 <abrahamgue05@gmail.com>
2025-02-02 18:37:46 -0600

    Changed files

--- Youtube/CS50x_2024_Lectures/Lecture_2_Arrays/Length.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <string.h>
-
-int main(void){
-    string name = get_string("Name: ");
-    int lenght = strlen(name);
-    printf("%i\n", lenght);
-}
--- Youtube/CS50x_2024_Lectures/Lecture_2_Arrays/scores.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int score1 = 72; //4 bytes
-    int score2 = 73; //4 bytes
-    int score3 = 33; //4 bytes
-
-    printf("Average: %.2f\n", (score1 + score2 + score3) / 3.0);
-}
