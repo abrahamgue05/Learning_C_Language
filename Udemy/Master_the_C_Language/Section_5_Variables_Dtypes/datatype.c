6220becb617157a753ca4625a4c1fd8ba396c82d
abraham05 <abrahamgue05@gmail.com>
2025-02-11 16:50:26 -0600

    Data type float, doube and bool in action

---
+++ Udemy/Master_the_C_Language/Section_5_Variables_Dtypes/datatype.c
@@ -0,0 +1,21 @@
+/*
+Author: Abraham Guerrero
+Purspose: See data type in action.
+Date 10/02/25 DD/MM/YY
+
+*/
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    float abraham = 23.333; 
+    double abraham2 = 55.555555e+11;
+    
+    _Bool boolvariable = 1;
+
+    bool myBoolead = true;
+    bool anotherBoolean = false;
+
+    return 0;
+}
