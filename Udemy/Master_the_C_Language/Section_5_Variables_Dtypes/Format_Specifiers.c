a7e86c32ca8750f097468782132d9205dd2ea7af
abraham05 <abrahamgue05@gmail.com>
2025-02-12 14:48:10 -0600

    Format specifier to print certain variables

---
+++ Udemy/Master_the_C_Language/Section_5_Variables_Dtypes/Format_Specifiers.c
@@ -0,0 +1,30 @@
+/*
+Author: Abraham Guerrero
+Purspose: Watch format specifier in action.
+Date 11/02/25 DD/MM/YY
+*/
+#include <stdio.h>
+
+int main() {
+    
+    int integerVar = 100;
+    float floatingVar = 331.79;
+    double doubleVar = 8.44e+11;
+    char charVar = 'W';
+
+    _Bool boolVar = 0;
+
+    printf("integerVar = %i\n", integerVar);// 100
+    printf("floatingVar = %f\n", floatingVar);// 331.790009
+    printf("doubleVar = %e\n", doubleVar);// 8.440000e+011
+    printf("doubleVar = %g\n", doubleVar);// 8.44e+011
+    printf("charVar = %c\n", charVar);// W
+
+    printf("boolVar = %i\n", boolVar);// 0
+
+    printf("doubleVar = %g %f\n", doubleVar,floatingVar);
+    // Prints two varibales 8.44e+011 and 331.790009
+    printf("floatingVar = %.2f\n", floatingVar);// 331.79
+
+    return 0;
+}
