e0dfc84288aed594f06ada86c2923489374fad3c
abraham05 <abrahamgue05@gmail.com>
2025-02-12 20:46:13 -0600

    agrc and argv in main function

---
+++ Udemy/Master_the_C_Language/Section_5_Variables_Dtypes/arguments.c
@@ -0,0 +1,20 @@
+/*
+Author: Abraham Guerrero
+Purspose: main functions arguments
+Date 12/02/25 DD/MM/YY
+
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    int numberOfArguments = argc;
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
+
+    printf("Number of Arguments: %d\n", numberOfArguments);
+    printf("Argument1 is the progra name: %s\n", argument1);
+    printf("Argument2 is the command line argument: %s\n", argument2);
+    
+    return 0;
+}
