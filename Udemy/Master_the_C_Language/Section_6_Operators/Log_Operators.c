371d073e4e817e67821cd5da6cd03b4f777db0f1
abraham05 <abrahamgue05@gmail.com>
2025-02-17 15:20:18 -0600

    Logical operators for true or false values

---
+++ Udemy/Master_the_C_Language/Section_6_Operators/Log_Operators.c
@@ -0,0 +1,27 @@
+/*
+Author: Abraham Guerrero
+Purspose: Look at logical operators in action.
+Date 13/02/25 DD/MM/YY
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    _Bool a = 1;
+    _Bool b = 1;
+    _Bool d = 0;
+    _Bool result1;
+    _Bool result2;
+    _Bool result3;
+
+    result1 = a && b;
+    result2 = d || d;
+    result3 = !d;
+
+    printf("When && result is: %d\n", result1); //1 Both are true
+    printf("When || result is: %d\n", result2); //0 Both are false or 0 
+    printf("When ! result is: %d", result3);// 1 Change operator
+
+    return 0;
+}
