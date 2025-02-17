92a1814fa7dc2de9b4861617780e30818452aaaf
abraham05 <abrahamgue05@gmail.com>
2025-02-17 15:18:40 -0600

    Arithmetic operators for operations

---
+++ Udemy/Master_the_C_Language/Section_6_Operators/Ari_Operators.c
@@ -0,0 +1,27 @@
+/*
+Author: Abraham Guerrero
+Purspose: Look at arithmetich operators in action. 
+Date: 13/02/25 DD/MM/YY
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    int a = 33;
+    int b = 15;
+    int result = 0;
+    
+    printf("With + result is: %d\n", result = a + b);// 48
+    printf("With - result is: %d\n", result = a - b);// 18
+    printf("With * result is: %d\n", result = a * b);// 495
+    printf("With / result is: %d\n", result = a / b);// 2
+    printf("With %% result is: %d\n", result = a % b);// 3
+    printf("With ++ after variableresult is: %d\n", result = a++);// 33 
+    printf("With ++ before variable result is: %d\n", result = ++a);// 35
+    printf("With -- after varibale result is: %d\n", result = --a);// 34
+    printf("With -- before variable result is: %d\n", result = a--);// 34
+
+
+    return 0;
+}
