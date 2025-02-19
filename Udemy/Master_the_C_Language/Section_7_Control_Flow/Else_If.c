382e75a105a77b5148432d84158143566be936d2
abraham05 <abrahamgue05@gmail.com>
2025-02-19 14:43:40 -0600

    Else if to see if a number is positive, negative or zero

---
+++ Udemy/Master_the_C_Language/Section_7_Control_Flow/Else_If.c
@@ -0,0 +1,24 @@
+/*
+Author: Abraham Guerrero
+Purspose: Look at else if in action
+Date: 17/02/25 DD/MM/YY
+*/
+#include <stdio.h>
+
+int main() {
+
+    int number, sign;
+
+    printf("Please type in a number: ");
+    scanf("%i", &number);
+
+    if(number < 0){// Check if numbers is negative
+        sign = -1;
+    }else if(number == 0){// Checks if number is 0
+        sign = 0;
+    }else// If number is not negative or 0 then is positive
+        sign = 1;
+
+    printf("Sign = %i\n", sign);
+    return 0;
+}
