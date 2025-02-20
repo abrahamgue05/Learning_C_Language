97efa4f26df7393fbeed42bc53714a2673744b2a
abraham05 <abrahamgue05@gmail.com>
2025-02-20 15:35:55 -0600

    Switch statement in sted of if cases

---
+++ Udemy/Master_the_C_Language/Section_7_Control_Flow/Switch.c
@@ -0,0 +1,29 @@
+/*
+Author: Abraham Guerrero
+Purspose: Look at switch statement in action.
+Date: 18/02/25 DD/MM/YY
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    enum Weekday today = Monday;
+
+    switch (today){// When the expression today is meet prints any of the cases.
+    case Sunday:
+        printf("Today is Sunday.");
+        break;
+    case Monday: 
+        printf("Today is Monday.");
+        break;
+    case Tuesday:
+        printf("Today is Tuesday.");
+        break;
+    default:
+        printf("Whatever");// If no expression meet it prints this.
+        break;
+    }
+    return 0;
+}
