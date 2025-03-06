4adb2c059c91648706128faf870c2230be1e50f2
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-05 22:58:08 -0600

    But I am loving both career

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/Strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    char * str= "Hi, read the commits messages for a story";
+
+    /*
+    char uses ''
+    char * i is a string
+
+    */
+
+    printf("%s\n", str);
+    return 0;
+}
