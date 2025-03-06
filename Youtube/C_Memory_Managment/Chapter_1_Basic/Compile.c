2c56679bdadbeeb7471d2460083ddd27e84bc792
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-05 22:52:04 -0600

    I am from Mexico and I am 19 YO

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/Compile.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("starting sneaklang tools\n");
+    //does_not_exist("uh oh"); undefined reference to `does_not_exist'
+    printf("finished sneklang tools\n");
+    return 0;
+}
