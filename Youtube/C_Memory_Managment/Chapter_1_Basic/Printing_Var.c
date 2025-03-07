bdbde890e266c04b3ede83fdddedb6792f2013a5
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-06 22:37:31 -0600

    I said it will be a story but is just gonna be random facts

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/Printing_Var.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 8;
+    char letter = 'A';
+    float floatnum = 3.1416;
+    char *str = "String";
+
+    printf("This print a number %d\n", num);
+    printf("This print a letter %c\n", letter);
+    printf("This print a float number %f\n", floatnum);
+    printf("This print a string %s\n", str);
+
+
+    return 0;
+}
