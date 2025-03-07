093bf39ce5facecc95d704eb2b8238c530ba238d
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-06 22:36:45 -0600

    I really love food and learning

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/Variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 5;
+
+    printf("That's declaring a variable %d\n", x);
+
+    x = 10;
+
+    printf("This changes its value but its still posible %d\n", x);
+
+    //float x = 12;
+    //printf("Now this is not posible %f\n", x);
+
+    return 0;
+}
