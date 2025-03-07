42a12410eb9bb8caea30d6f8c6ff042ec34ca810
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-06 22:41:04 -0600

    That's why when I started studing I started with Web Dev

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/Operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+    int num1 = 5;
+    int num2 = 10;
+
+    int operation = num1 + num2;// Just change the operator to: +, -, /, %, *
+    printf("There result of the operation is: %d\n", operation);
+
+}
