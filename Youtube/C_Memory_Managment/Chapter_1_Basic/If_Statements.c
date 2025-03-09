97ea6f655722264d482857bc260caab4a77cf02f
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-09 17:36:43 -0600

    I like linux, is the first time I use it daily

---
+++ Youtube/C_Memory_Managment/Chapter_1_Basic/If_Statements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    int x = 5;// Change this number to change the result.
+    
+    if (x > 3){
+        printf("The number is greater than 3");
+    }else if(x == 5){
+        printf("The number is equal");
+    }else{
+        printf("The number is less than 3");
+    }
+    
+}   
