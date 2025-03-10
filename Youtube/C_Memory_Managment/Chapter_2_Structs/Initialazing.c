3206e0172f4b774ba5bb3f5765c24844130251cd
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-10 10:03:16 -0600

    Giving values to a struct

---
+++ Youtube/C_Memory_Managment/Chapter_2_Structs/Initialazing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct City{// struct to initialize a struct followed by its name.
+  char *name;
+  int lat;
+  int lon;
+};
+
+
+
+int main(void){
+
+  struct City c = {0};// All values set to 0.
+
+  struct City C = {"San Fransisco", 37, -122};// All values set to each position.
+
+  struct City c ={// Giving value 1 by 1
+    .name = "San Fransisco",// The dot is to access the struct.
+    .lat = 7,
+    .lon = -122
+  };
+
+  struct City c;
+
+  c.lat = 37;
+
+}
