bd02523209e43bf2f1c4c1631cc21498b33a79af
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-10 10:04:31 -0600

    Printing values of a struct

---
+++ Youtube/C_Memory_Managment/Chapter_2_Structs/Printing.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct City{// struct to initialize a struct followed by its name.
+  char *name;
+  int lat;
+  int lon;
+};
+
+
+
+int main(void){
+
+  struct City c ={// Giving value 1 by 1
+    .name = "San Fransisco",// The dot is to access the struct.
+    .lat = 7,
+    .lon = -122
+  };
+
+  printf("City: %s\n", c.name);
+  printf("Lat: %d\n", c.lat);
+  printf("Lon: %d\n", c.lon);
+}
