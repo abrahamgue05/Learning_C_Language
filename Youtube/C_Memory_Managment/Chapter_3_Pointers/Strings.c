cf6af491f086df46f8e2ab9bd268cd7ed58e8009
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-14 16:28:43 -0600

    Declaring and printing a string

---
+++ Youtube/C_Memory_Managment/Chapter_3_Pointers/Strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  
+  char *msg = "ssh terminal.shop for the best coffee";
+
+  char str1[] = "Hi";
+  char *str2 = "Snek";
+
+  printf("%s\n %s \n%s\n", msg, str1, str2);
+}
