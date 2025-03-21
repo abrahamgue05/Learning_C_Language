2a85099ae87d6d0b335256959794b7f0cfa36a91
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-20 23:36:56 -0600

    Heaps

--- Youtube/C_Memory_Managment/Chapter_6_Stack_n_Heap/Heap.c
+++ Youtube/C_Memory_Managment/Chapter_6_Stack_n_Heap/Heap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int *new_int_array(int size){
-  int *new_arr = (int*)malloc(size * sizeof(int));// Allocate memory
+  int *new_arr = (int*)malloc(size * sizeof(char));// Allocate memory
   if (new_arr == NULL){
     printf(stderr, "Memory allocation failed\n");
     exit(1);// Exit if allocation fails
