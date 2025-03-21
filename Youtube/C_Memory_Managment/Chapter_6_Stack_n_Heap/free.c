e2f65b8a42abc8746a14bec8370ab2f6d751786e
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-20 23:27:01 -0600

    Using free()

---
+++ Youtube/C_Memory_Managment/Chapter_6_Stack_n_Heap/free.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+  const int num_list = 500;
+  for (int i = 0; i < num_list; i++){
+    int *lst = allocate_scalar_list(50000,2);
+    if (lst == NULL){
+      printf("Failed to allocate list\n");
+      return 1;
+    }else{
+      printf("Allocated list %d\n", i);
+      free(lst);
+    }
+  }
+  return 0;
+}
