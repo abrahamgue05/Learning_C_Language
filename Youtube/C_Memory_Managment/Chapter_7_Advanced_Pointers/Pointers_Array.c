ac5e94bd157e3f48696971e9b4631dd64eeebdff
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-21 22:12:24 -0600

    Array of pointers

---
+++ Youtube/C_Memory_Managment/Chapter_7_Advanced_Pointers/Pointers_Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char **string_array = malloc(sizeof(char *) * 3);
+    string_array[0] = "foo";
+    string_array[1] = "bar";
+    string_array[2] = "baz";
+
+    return 0;
+}
