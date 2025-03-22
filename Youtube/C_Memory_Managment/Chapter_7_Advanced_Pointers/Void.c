9d7d9b982eff653f4dc0585928b500445b18b96c
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-21 23:00:25 -0600

    Void as a generic ptr

---
+++ Youtube/C_Memory_Managment/Chapter_7_Advanced_Pointers/Void.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  int number = 42;// This can be anything, C trust.
+  void *generic_ptr = &number;
+
+  // This doesn't work, incomplete type "void" is not allowed
+  printf("Value of number: %d\n", *generic_ptr);
+
+  // This works: Cast to appropiate type before derefering
+  printf("Value of number: %d\n", *(int*)generic_ptr);
+
+    return 0;
+}
