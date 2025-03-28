6a7e548528b725ff9ed82291fcd4a9b5915b6ec1
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-28 11:35:45 -0600

    Clean

--- Youtube/C_Programming_Tutorial_for_begginers/If_Statements.c
+++ Youtube/C_Programming_Tutorial_for_begginers/If_Statements.c
@@ -3,7 +3,7 @@
 
 /*int max(int num1, int num2, int num3){
     int result;
-    if(num1 >= num2 && num1 >= num3){ //if(condition),{code} //&& Logical operetor AND,
+    if(num1 >= num2 && num1 >= num3){ //if(condition),{code} //&& Logical operator AND,
     both conditions are TRUE
         result = num1;
     }else if(num2 >= num1 && num2 >= num3){ //Check another condition if the first is false
@@ -19,7 +19,7 @@ int main() {
     // Programming structure which we can use to help the program to make decision
     //printf("%d", max(40,10,15));
 
-    if(3 > 2 || 2 > 5){ //|| Logical operetor OR, one of the conditions has to be TRUE
+    if(3 > 2 || 2 > 5){ //|| Logical operator OR, one of the conditions has to be TRUE
         printf("True");
     }else{
         printf("False");
