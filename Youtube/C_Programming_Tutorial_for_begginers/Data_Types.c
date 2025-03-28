905227ba3e27e745069e64c6cc61be824b71bf67
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-28 11:35:19 -0600

    Clean

--- Youtube/C_Programming_Tutorial_for_begginers/Data_Types.c
+++ Youtube/C_Programming_Tutorial_for_begginers/Data_Types.c
@@ -5,7 +5,7 @@ int main() {
     int age = 40; //Integers 
     double gpa = 40.0; //Decimal
     char grade = 'A';//Character, store just 1 character
-    char phrase[] = "Hello world";//String of caracters
+    char phrase[] = "Hello world";//String of caracteres
 
     return 0;
 }
