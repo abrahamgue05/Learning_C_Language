e15639488c6ce69f9e64c01f8aef146119942b2b
Abrahamgue05 <abrahamgue05@gmail.com>
2025-03-29 17:56:56 -0600

    Clean

--- Youtube/C_Programming_Tutorial_for_begginers/Variables.c
+++ Youtube/C_Programming_Tutorial_for_begginers/Variables.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-    // Variables: Container where we can store different types of data(number,text, caracters, etc)
+    // Variables: Container where we can store different types of data(number,text, characters, etc)
     char characterName[] = "Tom"; //Data type, variable name[], value; char = numbers and letters
     int characterAge = 67; //Data type, variable name, value, int = numbers
     
-    printf("There once was a man named %s\n", characterName); //%s instert the value of the variable(String)
-    printf("he was %d years old.\n", characterAge); //%d inster the value of the variable(number)
-    characterAge = 30; // Can change the variable values (will afect only lines down the new change)
+    printf("There once was a man named %s\n", characterName); //%s insert the value of the variable(String)
+    printf("he was %d years old.\n", characterAge); //%d insert the value of the variable(number)
+    characterAge = 30; // Can change the variable values (will affect only lines down the new change)
     printf("He really liked the name %s\n", characterName);
     printf("but did not like being %d\n", characterAge);
 }
