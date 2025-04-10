f3cc6755335e88240faf27d642739fc09a80b928
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-09 20:38:18 -0600

    If everything goes ok, I will be using gtk librarie

---
+++ Projects/calculator.c
@@ -0,0 +1,5 @@
+#include<gtk/gtk.h># Using gtk for the GUI part
+
+#include<stdlib.h># Memory allocation, process control, conversion, and more
+#include<string.h># Perform tasks on strings
+#include<stdbool.h># Manage the boolean variable
