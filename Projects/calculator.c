b0149e43c2f57a86b942ec9de315d9ee58f1970b
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-09 20:57:11 -0600

    Gtkwidgets basics

--- Projects/calculator.c
+++ Projects/calculator.c
@@ -1,5 +1,12 @@
+# Libraries
 #include<gtk/gtk.h># Using gtk for the GUI part
 
 #include<stdlib.h># Memory allocation, process control, conversion, and more
 #include<string.h># Perform tasks on strings
 #include<stdbool.h># Manage the boolean variable
+
+typedef struct{
+	GtkWidget *window;# Pointer to gtk window(scree)
+	GtkWidget *grid;# Grid lets to lay out child widgets
+	GtkWidget *button[17];
+}calc;# Gtk instructions for the calculator
