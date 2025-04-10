5f2a4cea1ea31cad812684d52b538ae14f6d3c91
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-09 21:02:38 -0600

    Variables

--- Projects/calculator.c
+++ Projects/calculator.c
@@ -10,3 +10,18 @@ typedef struct{
 	GtkWidget *grid;# Grid lets to lay out child widgets
 	GtkWidget *button[17];
 }calc;# Gtk instructions for the calculator
+
+GtkWidget *box;# Draws a box for the calculator
+
+# Variables
+#define SIZE 10# Max numbers to operate
+
+# Input and Output numbers on scree
+char input_buffer[100] = {0};
+char output_buffer[100] = {0};
+
+bool clear_buffer = false;# Cleans screen
+bool add=false, mul = false, divv = false, sub = false;# Operations
+float result = 0.0;# Result
+static float num[SIZE];# Store input numbers
+int count = 0;# Tracks numbers stored
