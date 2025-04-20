e91ee3cf1ae71062b3dfa6e55eb72cf9d3316960
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-19 21:25:13 -0600

    Logic

--- Projects/calculator.c
+++ Projects/calculator.c
@@ -13,43 +13,26 @@ typedef struct{
 
 GtkWidget *box; // Entry box to show input/output text
 
+// Variables
 #define SIZE 10
 
-char input_buffer[100] = {0};    // Holds the current number being typed
-char output_buffer[100] = {0};   // Holds the entire display content
+char input_buffer[100] = {0};// Holds the current number being typed
+char output_buffer[100] = {0};// Holds the entire display content
 
-bool clear_buffer = false;       // Flag to clear input buffer after operator
-bool add=false;                  // Addition operation flag
-bool mul = false;                // Multiplication operation flag
-bool divv = false;               // Division operation flag
-bool sub = false;                // Subtraction operation flag
+bool clear_buffer = false;// Flag to clear input buffer after operator
+bool add=false;// Addition operation flag
+bool mul = false;// Multiplication operation flag
+bool divv = false;// Division operation flag
+bool sub = false;// Subtraction operation flag
 
-float result = 0.0;              // Final result of the calculation
-static float num[SIZE];          // Array to hold numbers for calculation
-int count = 0;                   // Index for num[] to track how many values entered
+float result = 0.0;// Final result of the calculation
+static float num[SIZE];// Array to hold numbers for calculation
+int count = 0;// Index for num[] to track how many values entered
+
+static void calculate(GtkButton *button, gpointer data){
+	const gchar* text = gtk_button_get_label(button);// Get text from clicked button
 
 
-static void calculate(GtkButton *button, gpointer data){# Gets the label of clicked buttons
-	const gchar* text = gtk_button_get_label(button);
 
-	if( (strcmp("+", text)==0) ||   (strcmp("-", text)==0) ||  (strcmp("/", text)==0) ||  (strcmp("x", text)==0) ||  (strcmp("=", text)==0) ){
-		num[count] = atof(input_buffer);
-		count++;
-		clear_buffer = true;# Clear screen
-
-		# Operations
-		if(strcmp("+",text)==0){
-			add = true;
-		}
-		if(strcmp("-",text)==0){
-			sub = true;
-		}
-		if(strcmp("/",text)==0){
-			divv = true;
-		}
-		if(strcmp("x",text)==0){
-			mul = true;
-		}
-	}
 
 	
