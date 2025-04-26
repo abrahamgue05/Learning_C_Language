61d222172a074b5c8f91cb4388fad6abe6670418
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-25 22:36:44 -0600

    Snake Game

---
+++ Projects/Snake.c
@@ -0,0 +1,6 @@
+// Copyright EightSprites.
+// Live coding available at: https://www.youtube.com/watch?v=t3y2b2_moY8
+// This file is put in public domain.
+// You can use it as you like.
+
+// Libraries
