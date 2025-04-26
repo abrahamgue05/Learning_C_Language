de515b90ab58b694e4c1f03abd24246287ced97d
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-04-25 22:39:19 -0600

    COnstants

--- Projects/Snake.c
+++ Projects/Snake.c
@@ -9,3 +9,9 @@
 #include <stdlib.h>    // For rand, srand, system, etc.
 #include <conio.h>     // For getch() - captures keyboard input without Enter
 #include <time.h>      // For seeding the random number generator
+
+// Constants
+
+#define cols 25// Size of board
+#define rows 25// Size of boards
+#define foods 50// Food
