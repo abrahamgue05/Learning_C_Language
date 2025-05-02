5f3c2e36a53cebffaff1668de11944d60c69f08f
AbrahamGue05 <abrahamgue05@gmail.com.>
2025-05-01 21:05:21 -0600

    Left code school homework

---
+++ Projects/Left_Code.c
@@ -0,0 +1,13 @@
+// =========================
+// PIN CONFIGURATION
+// =========================
+
+// Motor pins
+const int IN1 = 2;   // Left motor direction
+const int ENA = 5;   // Left motor speed (PWM)
+const int IN2 = 4;   // Right motor direction
+const int ENB = 6;   // Right motor speed (PWM)
+
+// Ultrasonic sensor pins
+const int trigPin = 12;
+const int echoPin = 13;
